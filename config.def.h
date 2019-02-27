af647a8edab99e1627958e90c12e98c012f8f71a
Ned Pummeroy <npummeroy@atlassian.com>
2019-02-28 02:50:17 +1100

    alpha

--- config.def.h
+++ config.def.h
@@ -13,11 +13,18 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char urgbordercolor[]  = "#de3d2f";
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg          bg          border        */
 	[SchemeNorm] = { col_gray3,  col_gray1,  col_gray2      },
 	[SchemeSel]  = { col_gray4,  col_cyan,   col_cyan       },
 	[SchemeUrg]  = { selfgcolor, selbgcolor, urgbordercolor },
+};
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
