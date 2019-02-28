596fe7404187b26a18282b293a59d5c13eaac966
Ned Pummeroy <npummeroy@atlassian.com>
2019-02-28 14:16:52 +1100

    urgent tag highlight

--- config.def.h
+++ config.def.h
@@ -12,19 +12,20 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char urgbordercolor[]  = "#de3d2f";
+static const char urgcolor[]        = "#de3d2f";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg          bg          border        */
 	[SchemeNorm] = { col_gray3,  col_gray1,  col_gray2      },
 	[SchemeSel]  = { col_gray4,  col_cyan,   col_cyan       },
-	[SchemeUrg]  = { selfgcolor, selbgcolor, urgbordercolor },
+	[SchemeUrg]  = { selfgcolor, urgcolor,   urgcolor       },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+	[SchemeUrg]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
